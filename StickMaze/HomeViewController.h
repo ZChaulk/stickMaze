08860b6056a445052db1f56f6a1fea9ea8f8b44f
Zackary Neil Chaulk <znc028@kona.cs.mun.ca>
2015-03-20 15:06:29 -0230

    GLK View for main Menu

--- StickMaze/HomeViewController.h
+++ StickMaze/HomeViewController.h
@@ -6,11 +6,15 @@
 //  Copyright (c) 2015 Zackary Neil Chaulk. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <GLKit/GLKit.h>
 
 #import "GameViewController.h"
 #import "RecordsViewController.h"
-@interface HomeViewController : UIViewController<GameViewControllerDelegate>
+@interface HomeViewController : GLKViewController<GameViewControllerDelegate>
+- (void)setupGL;
+- (void)tearDownGL;
+- (void)setupOrthographicView;
+@property (strong, nonatomic) EAGLContext *context;
 
 - (IBAction)start:(id)sender;
 - (IBAction)records:(id)sender;
