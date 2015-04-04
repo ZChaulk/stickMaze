6464afd27dfef6d70185988a37d98ca133c51f06
Jacob Frederick Parsons <jfp855@caracara.cs.mun.ca>
2015-04-04 16:32:04 -0230

    added high scores but they need to be checked

---
+++ StickMaze/ScoreController.h
@@ -0,0 +1,13 @@
+//
+//  ScoreController.h
+//  StickMaze
+//
+//  Created by Jacob Frederick Parsons on 2015-04-04.
+//  Copyright (c) 2015 Zackary Neil Chaulk. All rights reserved.
+//
+
+#ifndef StickMaze_ScoreController_h
+#define StickMaze_ScoreController_h
+
+
+#endif
